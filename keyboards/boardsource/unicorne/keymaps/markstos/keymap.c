ab198c5ea3da7903f4ad354e899fcc02de1d9a0f
jsears <jsears@galtech.net>
2023-12-22 07:09:07 -0500

    First compile of markstos keymap

--- keyboards/boardsource/unicorne/keymaps/markstos/keymap.c
+++ keyboards/boardsource/unicorne/keymaps/markstos/keymap.c
@@ -1,3 +1,6 @@
+#include QMK_KEYBOARD_H
+
+extern uint8_t is_master;
 
 
 enum custom_keycodes {
@@ -6,6 +9,18 @@ enum custom_keycodes {
     RAISE,
     FUNC,
     BACKLIT
+};
+
+enum combos {
+  JK_ESC
+};
+
+const uint16_t PROGMEM jk_combo[] = {KC_J, KC_K, COMBO_END};
+
+combo_t key_combos[COMBO_COUNT] = {
+  // For Vim, put Escape on the home row
+  [JK_ESC]    = COMBO(jk_combo, KC_ESC),
+
 };
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
@@ -32,13 +47,8 @@ enum custom_keycodes {
 
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-// [0] = LAYOUT_split_3x6_3( \
-//       KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_BSPC,\
-//       KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                     KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_SCLN ,KC_QUOT,\
-//       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                     KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_ESC, \
-//                                          KC_LGUI, MO(1), KC_SPC,       KC_ENT,  MO(2),   KC_RALT \
-// ),
-[_QWERTY] = LAYOUT( \
+
+[_QWERTY] = LAYOUT_split_3x6_3( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_DEL,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -50,14 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       //`--------------------------'  `--------------------------'
   ),
 
-// [1] = LAYOUT_split_3x6_3( \
-  //  _______, KC_1,    KC_2,   KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,\
-  //  _______, KC_EXLM, KC_AT,  KC_HASH, KC_DLR,  KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,\
-  //  _______, _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,\
-  //                                     _______, _______, _______,    _______, _______, _______ \
-  // ),
-
-  [_LOWER] = LAYOUT( \
+  [_LOWER] = LAYOUT_split_3x6_3( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       _______, KC_EXLM, KC_AT,  KC_HASH, KC_DLR,  KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -68,16 +71,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                           KC_TRNS,  KC_TRNS, LOWER,    KC_TRNS, KC_TRNS, KC_COLON \
                                       //`--------------------------'  `--------------------------'
     ),
-
-
-//   [2] = LAYOUT_split_3x6_3( \
-//      QK_BOOT, _______, _______, _______, _______, _______,                       RGB_VAI, RGB_HUI, RGB_SAI, RGB_MOD, RGB_TOG,_______, \
-//      EE_CLR,  _______, _______, _______, _______, _______,                       RGB_VAD, RGB_HUD, RGB_SAD, RGB_RMOD, CK_TOGG,_______,\
-//      _______, _______, _______, _______, _______, _______,                       _______, _______, _______, _______, _______, _______,\
-//                                           _______, _______, _______, _______, _______, _______ \
-//   )
   
-  [_RAISE] = LAYOUT( \
+  [_RAISE] = LAYOUT_split_3x6_3( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       _______, KC_DEL , XXXXXXX, KC_UNDS, KC_PLUS, KC_PGUP,                      XXXXXXX, XXXXXXX, XXXXXXX, KC_BSLS, KC_PIPE,_______,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -89,13 +84,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       //`--------------------------'  `--------------------------'
   ),
 
-  [_FUNC] = LAYOUT( \
+  [_FUNC] = LAYOUT_split_3x6_3( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       _______, KC_F1  , KC_F2  , KC_F3   , KC_F4 ,  KC_F5 ,                     KC_F6   , KC_F7  , KC_F8  , KC_F9  , KC_F10 ,_______,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       _______, KC_F11 , KC_F12 , XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,_______,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, KC_CAPS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX , XXXXXXX, XXXXXXX, XXXXXXX, RESET  ,XXXXXXX,\
+      _______, KC_CAPS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                     XXXXXXX , XXXXXXX, XXXXXXX, XXXXXXX, _______,XXXXXXX,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, FUNC   , XXXXXXX\
                                       //`--------------------------'  `--------------------------'
---
+++ keyboards/boardsource/unicorne/keymaps/markstos/rules.mk
@@ -0,0 +1,13 @@
+SERIAL_DRIVER = vendor
+AUDIO_DRIVER = pwm_hardware
+POINTING_DEVICE_DRIVER = analog_joystick
+# markstos: enable media keys
+EXTRAKEY_ENABLE = yes
+
+# markstos: smaller file size, little down-side
+LINK_TIME_OPTIMIZATION_ENABLE = yes
+
+# markstos: for combos!
+COMBO_ENABLE = yes
+
+TAP_DANCE_ENABLE = no
