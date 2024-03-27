98f067f1e6b702455af35dccea45b8ea014eda34
Mael VILLERBU <mael.villerbu.ext@orange.com>
2024-03-27 10:48:14 +0100

    feat: copy KC_DOT from base to num layer

--- keyboards/splitkb/kyria/keymaps/tyosa/keymap.c
+++ keyboards/splitkb/kyria/keymaps/tyosa/keymap.c
@@ -169,7 +169,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_NUM ] = LAYOUT(
       XXXXXXX, KC_LBRC,    KC_7,    KC_8,    KC_9, KC_RBRC,                                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
       XXXXXXX, KC_SCLN,    KC_4,    KC_5,    KC_6,  KC_EQL,                                     XXXXXXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI, XXXXXXX,
-      XXXXXXX,  KC_GRV,    KC_1,    KC_2,    KC_3, KC_BSLS, _______, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX,  KC_GRV,    KC_1,    KC_2,    KC_3, KC_BSLS, _______, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_DOT, XXXXXXX, XXXXXXX,
                                  _______, _______,  KC_DOT,    KC_0, KC_MINS, _______, _______, _______, _______, _______
     ),
 
