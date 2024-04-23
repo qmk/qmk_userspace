726b591d94b9e59d63f84f967f8f29474d709d0a
Jose Gisbert <jisbert@users.noreply.github.com>
2024-04-23 14:24:32 +0200

    Update copyright notice

--- keyboards/splitkb/kyria/rev1/keymaps/lejouson/config.h
+++ keyboards/splitkb/kyria/rev1/keymaps/lejouson/config.h
@@ -1,18 +1,5 @@
-/* Copyright 2019 Thomas Baart <thomas@splitkb.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+// Copyright 2024 Jose Gisbert, aka lejouson <email@gont.es>
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 #pragma once
 
@@ -35,7 +22,3 @@
 #ifdef ENCODER_ENABLE
   #define ENCODER_RESOLUTION 2
 #endif
-
-// If you are using an Elite C rev3 on the slave side, uncomment the lines below:
-// #define SPLIT_USB_DETECT
-// #define NO_USB_STARTUP_CHECK
