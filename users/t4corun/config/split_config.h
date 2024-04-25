f7b6532483ce78e5ce6117bd92544aa7c37c6f3d
Victor <VictorN076@gmail.com>
2024-04-24 19:35:43 -0500

    More cleanup - added more time for config layer to activate - Cleaned up unused code (e.g. combos, split config) - removed uneeded oled driver include

--- users/t4corun/config/split_config.h
+++ users/t4corun/config/split_config.h
@@ -4,8 +4,15 @@
 *
 * https://docs.qmk.fm/#/feature_split_keyboard
 * 
+*
+* needed for for the rgb matrix indicators to work
+* - SPLIT_LAYER_STATE_ENABLE
+* - SPLIT_LED_STATE_ENABLE
+* - SPLIT_MODS_ENABLE
+* 
 */
 
+//Clear variables we plan to use that might be set elsewhere
 #undef SPLIT_TRANSPORT_MIRROR
 #undef SPLIT_LAYER_STATE_ENABLE
 #undef SPLIT_LED_STATE_ENABLE
@@ -14,14 +21,9 @@
 #undef SPLIT_ACTIVITY_ENABLE
 #undef SPLIT_USB_DETECT
 
-
-
+//Set common configuration for all keyboards
 #define SPLIT_TRANSPORT_MIRROR
-//#define SPLIT_LAYER_STATE_ENABLE
-//#define SPLIT_LED_STATE_ENABLE
-//#define SPLIT_MODS_ENABLE
 #define SPLIT_OLED_ENABLE
 #define SPLIT_ACTIVITY_ENABLE
-//#   define SPLIT_USB_DETECT
 
 #define MASTER_RIGHT
