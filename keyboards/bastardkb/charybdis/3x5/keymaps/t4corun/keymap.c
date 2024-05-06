61e49576f2e9642778d9c4404a9597f0be12403f
Victor <VictorN076@gmail.com>
2024-05-05 23:23:35 -0500

    Forgot to undo the mouse button changes on the keymap

--- keyboards/bastardkb/charybdis/3x5/keymaps/t4corun/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/t4corun/keymap.c
@@ -12,7 +12,7 @@ LAYOUT_cnano_wrapper ( \
 	k00, k01, k02,     k03, k04, k05, k06, k07, k08, k09, \
 	k10, k11, k12,     k13, k14, k15, k16, k17, k18, k19, \
 	k20, k21, k22,     k23, k24, k25, k26, k27, k28, k29, \
-	          TR_BTN1, k33, k34, k35, k36                 \
+	          KC_BTN1, k33, k34, k35, k36                 \
 )
 
 #define CNANO(...) LAYOUT_3x5_3_keymap(__VA_ARGS__)
--- keyboards/crkbd/keymaps/t4corun/keymap.c
+++ keyboards/crkbd/keymaps/t4corun/keymap.c
@@ -12,7 +12,7 @@ LAYOUT_crkbd_wrapper ( \
 	KC_NO, k00, k01, k02,     k03, k04, k05, k06, k07,    k08, k09, KC_NO, \
 	KC_NO, k10, k11, k12,     k13, k14, k15, k16, k17,    k18, k19, KC_NO, \
 	KC_NO, k20, k21, k22,     k23, k24, k25, k26, k27,    k28, k29, KC_NO, \
-	                 TR_BTN1, k33, k34, k35, k36, TR_BTN4                  \
+	                 KC_BTN1, k33, k34, k35, k36, KC_BTN4                  \
 )
 
 #define CRKBD(...) LAYOUT_3x5_3_keymap(__VA_ARGS__)
