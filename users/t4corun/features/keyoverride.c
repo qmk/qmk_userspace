f8d8e3fdee859a6705c4fdc80101f67e42fb5fb7
Victor <VictorN076@gmail.com>
2024-05-10 07:47:57 -0500

    Did not do the bitmask properly - Now the function keys should work on the number layer - Also made the media next key only work on navigation layer

--- users/t4corun/features/keyoverride.c
+++ users/t4corun/features/keyoverride.c
@@ -13,20 +13,21 @@ Intentionally did not convert all the override/autoshit tap holds to this format
 
 
 const key_override_t delete_override     = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
-const key_override_t media_next_override = ko_make_basic(MOD_MASK_SHIFT, KC_MNXT, KC_MPRV);
 
-const key_override_t f1_override         = ko_make_with_layers(MOD_MASK_CSA, KC_1,    KC_F1,  _NUMBER);
-const key_override_t f2_override         = ko_make_with_layers(MOD_MASK_CSA, KC_2,    KC_F2,  _NUMBER);
-const key_override_t f3_override         = ko_make_with_layers(MOD_MASK_CSA, KC_3,    KC_F3,  _NUMBER);
-const key_override_t f4_override         = ko_make_with_layers(MOD_MASK_CSA, KC_4,    KC_F4,  _NUMBER);
-const key_override_t f5_override         = ko_make_with_layers(MOD_MASK_CSA, KC_5,    KC_F5,  _NUMBER);
-const key_override_t f6_override         = ko_make_with_layers(MOD_MASK_CSA, KC_6,    KC_F6,  _NUMBER);
-const key_override_t f7_override         = ko_make_with_layers(MOD_MASK_CSA, KC_7,    KC_F7,  _NUMBER);
-const key_override_t f8_override         = ko_make_with_layers(MOD_MASK_CSA, KC_8,    KC_F8,  _NUMBER);
-const key_override_t f9_override         = ko_make_with_layers(MOD_MASK_CSA, KC_9,    KC_F9,  _NUMBER);
-const key_override_t f10_override        = ko_make_with_layers(MOD_MASK_CSA, KC_COMM, KC_F10, _NUMBER);
-const key_override_t f11_override        = ko_make_with_layers(MOD_MASK_CSA, KC_DOT,  KC_F11, _NUMBER);
-const key_override_t f12_override        = ko_make_with_layers(MOD_MASK_CSA, TR_MINS, KC_F12, _NUMBER);
+const key_override_t media_next_override = ko_make_with_layers(MOD_MASK_SHIFT, KC_MNXT, KC_MPRV, 1<<_NAVIGATION);
+
+const key_override_t f1_override         = ko_make_with_layers(MOD_MASK_CSA,   KC_1,    KC_F1,   1<<_NUMBER);
+const key_override_t f2_override         = ko_make_with_layers(MOD_MASK_CSA,   KC_2,    KC_F2,   1<<_NUMBER);
+const key_override_t f3_override         = ko_make_with_layers(MOD_MASK_CSA,   KC_3,    KC_F3,   1<<_NUMBER);
+const key_override_t f4_override         = ko_make_with_layers(MOD_MASK_CSA,   KC_4,    KC_F4,   1<<_NUMBER);
+const key_override_t f5_override         = ko_make_with_layers(MOD_MASK_CSA,   KC_5,    KC_F5,   1<<_NUMBER);
+const key_override_t f6_override         = ko_make_with_layers(MOD_MASK_CSA,   KC_6,    KC_F6,   1<<_NUMBER);
+const key_override_t f7_override         = ko_make_with_layers(MOD_MASK_CSA,   KC_7,    KC_F7,   1<<_NUMBER);
+const key_override_t f8_override         = ko_make_with_layers(MOD_MASK_CSA,   KC_8,    KC_F8,   1<<_NUMBER);
+const key_override_t f9_override         = ko_make_with_layers(MOD_MASK_CSA,   KC_9,    KC_F9,   1<<_NUMBER);
+const key_override_t f10_override        = ko_make_with_layers(MOD_MASK_CSA,   KC_COMM, KC_F10,  1<<_NUMBER);
+const key_override_t f11_override        = ko_make_with_layers(MOD_MASK_CSA,   KC_DOT,  KC_F11,  1<<_NUMBER);
+const key_override_t f12_override        = ko_make_with_layers(MOD_MASK_CSA,   TR_MINS, KC_F12,  1<<_NUMBER);
 
 const key_override_t **key_overrides = (const key_override_t *[]) {
 
