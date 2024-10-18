213097e1eef73ed82b84eab4c90e037a884685ad
Chris Zervakis <xrist.zerv@protonmail.com>
2024-10-18 21:10:12 +0300

    Enable LTO, disable more unused features

--- keyboards/keebio/iris_ce/keymaps/chzerv/rules.mk
+++ keyboards/keebio/iris_ce/keymaps/chzerv/rules.mk
@@ -1,14 +1,15 @@
-# LTO_ENABLE = yes
-
+LTO_ENABLE = yes
 # VIA_ENABLE = yes
 
 TRI_LAYER_ENABLE = yes
 CAPS_WORD_ENABLE = yes
 DYNAMIC_TAPPING_TERM_ENABLE = yes
-NRKO_ENABLE = yes
 
 SRC += features/select_word.c
 
 # Diasble features we don't use to reduce firmware size
 COMMAND_ENABLE = no
 MAGIC_ENABLE = no
+AUDIO_ENABLE = no
+BLUETOOTH_ENABLE = no
+CONSOLE_ENABLE = no
